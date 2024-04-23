7bacc57ffa36b9a86d8010374d6d78a8a72ae7f9
d5ng <dong5ffice@gmail.com>
2024-04-23 17:18:40 +0900

    Feat: add TIL Service

--- backend/app.ts
+++ backend/app.ts
@@ -19,11 +19,11 @@ app.use((req, res, next) => {
 })
 
 app.use((error: HttpError, req: Request, res: Response, next: NextFunction) => {
-  if (res.headersSent) {
-    return next(error)
-  }
-
+  if (res.headersSent) return next(error)
   res.status(error.code || 500).json({ message: error.message || "An unknown error occurred!" })
 })
 
-mongoose.connect(MONGODB_URI).then(() => app.listen(PORT, () => console.log(`listening at PORT ${PORT}`)))
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => app.listen(PORT, () => console.log(`listening at PORT ${PORT}`)))
+  .catch((error) => console.log(error))
--- backend/controllers/til-controller.ts
+++ backend/controllers/til-controller.ts
@@ -1,11 +1,12 @@
 import { RequestHandler } from "express"
-import tilServiceInstance from "../services/til-service"
 import HttpError from "../utils/http-error"
+import { TILServiceInstance } from "../services"
+import { TilData } from "../types/til.type"
 
 const getTilPosts: RequestHandler = async (req, res, next) => {
   try {
-    const data = await tilServiceInstance.allPosts()
-    return data
+    const data = await TILServiceInstance.getAllPosts()
+    return res.status(200).json({ data })
   } catch (error) {
     if (error instanceof HttpError) {
       return {
@@ -23,8 +24,8 @@ const getTilPosts: RequestHandler = async (req, res, next) => {
 
 const getTilPost: RequestHandler = async (req, res, next) => {
   try {
-    const data = await tilServiceInstance.allPost(req.params.tilId)
-    return data
+    const data = await TILServiceInstance.getPostById(req.params.tilId)
+    return res.status(200).json({ data })
   } catch (error) {
     if (error instanceof HttpError) {
       return {
@@ -40,8 +41,50 @@ const getTilPost: RequestHandler = async (req, res, next) => {
   }
 }
 
-const createTilPost: RequestHandler = (req, res, next) => {}
+const createTilPost: RequestHandler = async (req, res, next) => {
+  const body = req.body as TilData
 
-const deleteTilPost: RequestHandler = (req, res, next) => {}
+  if (!body.title || !body.contents) {
+    throw new HttpError("제목과 내용을 필수로 입력해주세요", 400)
+  }
+
+  try {
+    const data = await TILServiceInstance.createPost(body)
+    return res.status(200).json({ data })
+  } catch (error) {
+    if (error instanceof HttpError) {
+      return {
+        success: false,
+        errorMessage: error.message,
+      }
+    }
+
+    return {
+      success: false,
+      errorMessage: "알 수 없는 오류로 요청에 실패했어요",
+    }
+  }
+}
+
+const deleteTilPost: RequestHandler = async (req, res, next) => {
+  const tilId = req.params.tilId
+
+  try {
+    const data = await TILServiceInstance.deletePost(tilId)
+    return res.status(200).json({ data })
+  } catch (error) {
+    if (error instanceof HttpError) {
+      return {
+        success: false,
+        errorMessage: error.message,
+      }
+    }
+
+    return {
+      success: false,
+      errorMessage: "알 수 없는 오류로 요청에 실패했어요",
+    }
+  }
+}
 
 export { getTilPost, getTilPosts, createTilPost, deleteTilPost }
